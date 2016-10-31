b12dcb21bcedcb88908a3182a9818ab234526fc0
hityangxu <hityangxu@163.com>
2016-11-01 00:15:11 +0800

    refactory

--- app/app.component.js
+++ app/app.component.js
@@ -1,3 +1,6 @@
+/**
+ * Created by yangxu on 16-11-1.
+ */
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -8,35 +11,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-/**
- * Created by yangxu on 16-10-30.
- */
-var core_1 = require('@angular/core');
-var hero_service_1 = require("./hero.service");
+var core_1 = require("@angular/core");
 var AppComponent = (function () {
-    function AppComponent(heroService) {
-        this.heroService = heroService;
-        this.title = 'Tour of Heroes';
-        this.heroService = heroService;
+    function AppComponent() {
     }
-    AppComponent.prototype.ngOnInit = function () {
-        this.getHeroes();
-    };
-    AppComponent.prototype.getHeroes = function () {
-        var _this = this;
-        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
-    };
-    AppComponent.prototype.onSelect = function (hero) {
-        this.selectedHero = hero;
-    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n        <h1>{{title}}</h1>\n\n        <h2>My Heroes</h2>\n        <ul class=\"heroes\">\n            <li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n                <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n            </li>\n        </ul>\n        \n        <my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n    ",
-            providers: [hero_service_1.HeroService],
-            styles: [" .selected { background-color: #CFD8DC !important; color: white; } .heroes { margin: 0 0 2em 0; list-style-type: none; padding: 0; width: 15em; } .heroes li { cursor: pointer; position: relative; left: 0; background-color: #EEE; margin: .5em; padding: .3em 0; height: 1.6em; border-radius: 4px; } .heroes li.selected:hover { background-color: #BBD8DC !important; color: white; } .heroes li:hover { color: #607D8B; background-color: #DDD; left: .1em; } .heroes .text { position: relative; top: -3px; } .heroes .badge { display: inline-block; font-size: small; color: white; padding: 0.8em 0.7em 0 0.7em; background-color: #607D8B; line-height: 1em; position: relative; left: -1px; top: -4px; height: 1.8em; margin-right: .8em; border-radius: 4px 0 0 4px; } "]
+            template: "\n        <h1>{{title}}</h1>\n        <my-heroes></my-heroes>\n    "
         }), 
-        __metadata('design:paramtypes', [hero_service_1.HeroService])
+        __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
--- app/app.module.ts
+++ app/app.module.ts
@@ -7,6 +7,8 @@ import {FormsModule} from '@angular/forms'
 
 import {AppComponent} from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from "./heroes.component";
+import {HeroService} from "./hero.service";
 
 @NgModule({
     imports: [
@@ -15,7 +17,11 @@ import {HeroDetailComponent} from './hero-detail.component';
     ],
     declarations: [
         AppComponent,
-        HeroDetailComponent
+        HeroDetailComponent,
+        HeroesComponent
+    ],
+    providers: [
+        HeroService
     ],
     bootstrap: [AppComponent]
 })

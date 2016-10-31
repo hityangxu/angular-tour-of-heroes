6881741dc63c44bd9c0f72ef72928f76ba122527
hityangxu <hityangxu@163.com>
2016-11-01 01:07:29 +0800

    add detail router

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -5,6 +5,7 @@
 import {Component, OnInit} from "@angular/core";
 import {Hero} from "./hero";
 import {HeroService} from "./hero.service";
+import {Router} from "@angular/router";
 @Component({
     module: module.id,
     selector: 'my-dashboard',
@@ -13,14 +14,18 @@ import {HeroService} from "./hero.service";
 export class DashboardComponent implements OnInit {
     heroes: Hero[];
 
-
-    constructor(private heroService: HeroService) {
+    constructor(
+        private heroService: HeroService,
+        private router: Router
+    ) {
     }
-
 
     ngOnInit(): void {
         this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
     }
 
-    gotoDetail(hero: Hero): void { /* not implemented yet */}
+    gotoDetail(hero: Hero): void {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
+    }
 }

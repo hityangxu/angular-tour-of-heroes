ecec118b4e0dbe1304a65699c9489a4f1951f72b
hityangxu <hityangxu@163.com>
2016-11-05 00:26:39 +0800

    finish

---
+++ app/hero.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yangxu on 16-10-30.
+ */
+"use strict";
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
